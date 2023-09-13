52208d194c5916431d8c70d727f0d9eea3a63823
Manuel Santos <manueljoaosantos@gmail.com>
2023-09-13 15:15:44 +0100

    feat: Create avro send string

---
+++ Mjos.Avro.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+using System.Text.Json;
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "dotnet-users-string",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using(var consumer = new ConsumerBuilder<Null, string>(consumerConfig).Build())
+{
+    consumer.Subscribe("users-string");
+    while(true)
+    {
+        var consumerResult = consumer.Consume();
+        Console.WriteLine($"Received User: {consumerResult.Message.Value}");
+    }
+}
---
+++ Mjos.Avro.Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+using System;
+using System.Text.Json;
+
+var producerConfig = new ProducerConfig
+{ 
+    BootstrapServers = "localhost:9092"
+};
+
+var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+
+while (true)
+{
+    var userName =Faker.Name.FirstName();
+    Console.WriteLine($"Creating User: {userName}");
+    var result = producer.ProduceAsync("users-string", new Message<Null, string> { Value = userName }).GetAwaiter().GetResult();
+
+    Thread.Sleep(1000);
+}
